db6b0547167e5625b71d984481ff9ecdd499e6c7
OrphanCat <OrphanCat@8d86a4c2-b332-11de-b5b5-dfb26eda0927>
2014-06-29 10:33:35 +0000

    Added: Branch 11.4 before trunk is filled with 11.5
    git-svn-id: http://thtmlviewer.googlecode.com/svn@469 8d86a4c2-b332-11de-b5b5-dfb26eda0927

---
+++ branches/11.4/Demos/Main Demos/Htmlabt.dfm
@@ -0,0 +1,55 @@
+object AboutBox: TAboutBox
+  Left = 740
+  Top = 213
+  BorderIcons = [biSystemMenu]
+  Caption = 'About'
+  ClientHeight = 253
+  ClientWidth = 359
+  Color = clBtnFace
+  Constraints.MinHeight = 265
+  Constraints.MinWidth = 375
+  Font.Charset = DEFAULT_CHARSET
+  Font.Color = clWindowText
+  Font.Height = -13
+  Font.Name = 'System'
+  Font.Style = []
+  OldCreateOrder = True
+  Position = poScreenCenter
+  DesignSize = (
+    359
+    253)
+  PixelsPerInch = 96
+  TextHeight = 16
+  object BitBtn1: TBitBtn
+    Left = 276
+    Top = 220
+    Width = 75
+    Height = 25
+    Anchors = [akRight, akBottom]
+    Kind = bkOK
+    NumGlyphs = 2
+    TabOrder = 0
+  end
+  object Viewer: THtmlViewer
+    Left = 8
+    Top = 8
+    Width = 352
+    Height = 206
+    Enabled = False
+    TabOrder = 1
+    Anchors = [akLeft, akTop, akRight, akBottom]
+    BorderStyle = htNone
+    DefFontName = 'Times New Roman'
+    DefPreFontName = 'Courier New'
+    HistoryMaxCount = 0
+    HtOptions = []
+    NoSelect = True
+    PrintMarginBottom = 2.000000000000000000
+    PrintMarginLeft = 2.000000000000000000
+    PrintMarginRight = 2.000000000000000000
+    PrintMarginTop = 2.000000000000000000
+    PrintScale = 1.000000000000000000
+    ScrollBars = ssNone
+    ViewImages = False
+  end
+end
---
+++ branches/11.4/Demos/Other Demos/FrameBrowserUsingIndy10/HttpAsyncId10.pas
@@ -0,0 +1,121 @@
+unit HttpAsyncId10;
+{Use thread to make Get Asynchronous }
+
+interface
+{$include htmlcons.inc}
+{$include options.inc}
+uses
+  {$ifdef UseZLib}
+  IdCompressorZLib,
+  {$endif}
+  {$ifdef UseSSL}
+  Classes, IdHTTP, IdComponent, SysUtils, IdSSLOpenSSL;
+  {$else}
+  Classes, IdHTTP, IdComponent, SysUtils;
+  {$endif}
+
+type
+  THTTPAsync = class(TThread)
+  private
+    { Private declarations }
+    {$ifdef UseSSL}
+    SSL: TIdSSLIOHandlerSocketOpenSSL;
+    {$endif}
+    {$ifdef UseZLib}
+    Comp: TIdCompressorZLib;
+    {$endif}
+  protected
+    procedure Execute; override;
+    procedure WorkHandler(ASender: TObject; AWorkMode: TWorkMode; AWorkCount: Int64);
+  public
+    Url: string;
+    Stream: TMemoryStream;
+    HTTP: TIdHTTP;
+
+    constructor Create;
+    destructor Destroy; override;
+  end;
+
+implementation
+uses IdURI;
+
+{ Important: Methods and properties of objects in VCL can only be used in a
+  method called using Synchronize, for example,
+
+      Synchronize(UpdateCaption);
+
+  and UpdateCaption could look like,
+
+    procedure THTTPAsync.UpdateCaption;
+    begin
+      Form1.Caption := 'Updated in a thread';
+    end; }
+
+{ THTTPAsync }
+
+constructor THTTPAsync.Create;
+begin
+  inherited Create(True);
+  FreeOnTerminate := True;
+  Stream := TMemoryStream.Create;
+
+  HTTP := TIdHTTP.Create(Nil);
+  HTTP.HTTPOptions := HTTP.HTTPOptions + [hoNoParseMetaHTTPEquiv];
+  HTTP.HandleRedirects := True;
+  HTTP.ProtocolVersion := pv1_0;
+
+{$ifdef UseSSL}
+//  if Pos('https', Lowercase(Url)) > 0 then
+//  begin
+    SSL := TIdSSLIOHandlerSocketOpenSSL.Create(Nil);
+    SSL.SSLOptions.Method := sslvSSLv23;
+    SSL.SSLOptions.Mode := sslmClient;
+    HTTP.IOHandler := SSL;
+//  end;
+{$endif}
+  {$ifdef UseZLib}
+  Comp := TIdCompressorZLib.Create;
+  HTTP.Compressor := Comp;
+  {$endif}
+  HTTP.OnWork := WorkHandler;
+end;
+
+destructor THTTPAsync.Destroy;
+begin
+  Stream.Free;
+  Stream := Nil;
+{$ifdef UseSSL}
+  SSL.Free;
+{$endif}
+  HTTP.Free;
+  {$ifdef UseZLib}
+  Comp.Free;
+  {$endif}
+  inherited;
+end;
+
+procedure THTTPAsync.Execute;
+begin
+  try
+    //URI's should be encoded as the pathes might contain spaces.
+    //Verified at http://www.easyjet.com/en
+    HTTP.Get( TIdURI.URLEncode( Url ), Stream);
+    if Terminated then
+      OnTerminate := Nil;
+  except
+    HTTP.Disconnect;
+  end;
+end;
+
+procedure THTTPAsync.WorkHandler(ASender: TObject; AWorkMode: TWorkMode;
+   AWorkCount: Int64);
+begin
+  if Terminated then
+  begin
+    OnTerminate := Nil;
+    HTTP.Disconnect;
+  end;
+end;
+
+end.
+
